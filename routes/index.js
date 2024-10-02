4b4c65b0e7cfa93fc86ac1d88b650876dd4b358c
Your Name <you@example.com>
2024-10-02 15:41:20 +0200

    end of lesson

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.end('Strona Glowna');
 });
+router.get('/json', function(req, res){
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.end('{"json":{"servers":{"users"{"user"{"name":"test","id":1}}}}}');
+});
+router.get('/htmlGen', function(req, res){
+  var html = '<H1>Test page</H1><div>node js from code</div>'
+	res.end(html);
+})
 
 module.exports = router;
